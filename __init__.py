b325c9098b2c4960f4470e7843d76053775088b3
yinziyan1206 <408856732@qq.com>
2021-06-19 11:57:28 +0800

    init

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 __author__ = 'ziyan.yin'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 from .server import main as proxy
 
--- server.py
+++ server.py
@@ -4,8 +4,6 @@ import logging
 import socket
 import threading
 
-from . import utils
-
 TIMEOUT = 1000
 BUF_SIZE = 4096
 HOST = '0.0.0.0'
@@ -14,12 +12,43 @@ PORT = 11212
 _logger = logging.getLogger('proxy')
 
 
+def get_addr(package):
+    data = package.split(b'\r\n')
+    method = data[0]
+    is_ssl = False
+    if method.startswith(b'CONNECT'):
+        addr = method.split(b' ')[1].decode()
+        if ':' in addr:
+            host = addr[:addr.find(':')]
+            port = int(addr[addr.find(':') + 1:])
+        else:
+            host = addr
+            port = 443
+        is_ssl = True
+    else:
+        for header in data:
+            if header.startswith(b'Host'):
+                addr = header.split(b' ')[1].decode()
+                break
+        else:
+            addr = method.split(b'/')[2].decode()
+
+        if ':' in addr:
+            host = addr[:addr.find(':')]
+            port = int(addr[addr.find(':')+1:])
+        else:
+            host = addr
+            port = 80
+    protocol = method.split(b' ')[2].decode()
+    return host, port, is_ssl, protocol
+
+
 def communicate(client, server):
     try:
         while data := client.recv(BUF_SIZE):
             server.sendall(data)
-    except ConnectionError:
-        pass
+    except Exception as ex:
+        _logger.error(ex)
     finally:
         server.close()
 
@@ -39,13 +68,13 @@ def handle(client):
         client.close()
         return
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host, port, is_ssl, protocol = utils.get_addr(message)
+    host, port, is_ssl, protocol = get_addr(message)
     _logger.info((host, port))
     try:
         server.connect((host, port))
         server.settimeout(TIMEOUT)
         if is_ssl:
-            data = b"HTTP/1.0 200 Connection Established\r\n\r\n"
+            data = f"{protocol} 200 Connection Established\r\n\r\n".encode()
             client.sendall(data)
             threading.Thread(target=communicate, args=(client, server)).start()
             threading.Thread(target=communicate, args=(server, client)).start()
@@ -68,3 +97,7 @@ def main(ip, port):
         conn, addr = server.accept()
         _logger.debug(addr)
         threading.Thread(target=handle, args=(conn,)).start()
+
+
+if __name__ == '__main__':
+    main(HOST, PORT)
